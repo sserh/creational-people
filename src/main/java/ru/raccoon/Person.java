d8b6b3f24ef174df8935a3129ac02bd391b1ce49
sserh <serhios@bk.ru>
2023-09-20 17:00:09 +0700

    ver 1.0

---
+++ src/main/java/ru/raccoon/Person.java
@@ -0,0 +1,79 @@
+package ru.raccoon;
+
+public class Person {
+    protected final String name;
+    protected final String surname;
+    protected int age;
+    protected String address;
+
+    public Person(String name, String surname, int age, String address) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.address = address;
+    }
+
+    public boolean hasAge() {
+        return age >= 0;
+    }
+
+    public boolean hasAddress() {
+        return address != null;
+    }
+
+    public void happyBirthday() {
+        if (hasAge()) {
+            age++;
+        } else {
+            System.out.println("Выполнение метода на возможно, так как возраст человека неизвестен.");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public PersonBuilder newChildBuilder() {
+        PersonBuilder personBuilder = new PersonBuilder();
+        personBuilder
+                .setSurname(surname)
+                .setAge(5)
+                .setAddress(address);
+        return personBuilder;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + name
+                + " " + surname
+                + " Возраст: " + (hasAge() ? getAge() : "unknown")
+                + " Адрес: " + (hasAddress() ? getAddress() : "unknown")
+                + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        String s = name + surname;
+        for (int i = 0; i < s.length(); i++) {
+            hash += s.charAt(i);
+        }
+        return hash;
+    }
+}
---
+++ src/main/java/ru/raccoon/PersonBuilder.java
@@ -0,0 +1,36 @@
+package ru.raccoon;
+
+public class PersonBuilder {
+    private String name;
+    private String surname;
+    private int age = -1; //по умолчанию возраст не указан
+    private String address;
+
+    public PersonBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+    public PersonBuilder setSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
+    public PersonBuilder setAge(int age) {
+        if (age >= 0) {
+            this.age = age;
+        } else {
+            throw new IllegalArgumentException("Возраст не может быть отрицательным");
+        }
+        return this;
+    }
+    public PersonBuilder setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public Person build() {
+        if (name == null || surname == null) {
+            throw new IllegalStateException("Не указаны имя или фамилия человека");
+        }
+        return new Person(name, surname, age, address);
+    }
+}
